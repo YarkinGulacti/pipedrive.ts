6878540c07edfc4d304f0d11801b02177468a9ec
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2023-01-18 09:24:13 +0300

    Client tests added.

--- src/core/PipedriveClient.spec.ts
+++ src/core/PipedriveClient.spec.ts
@@ -1,12 +1,16 @@
-import { it, describe, test, expect } from "@jest/globals";
+import { describe, test, expect } from "@jest/globals";
 import { PipedriveClient } from "./PipedriveClient";
 
-it("Pipedrive Client tests.", () => {
-    describe("Pipedrive Client instantiation tests.", () => {
-        test("Pipedrive Client apiToken set test.", () => {
-            const client = new PipedriveClient();
+describe("Pipedrive Client tests.", () => {
+    test("Pipedrive Client apiToken set test.", () => {
+        const client = new PipedriveClient();
 
-            expect(client.apiToken).toBe(undefined);
-        })
-    })
+        expect(client.apiToken).toBeUndefined();
+
+        const token = "asdasdasdasdasdasdasd";
+        client.apiToken = token;
+
+        expect(client.apiToken).toBeDefined();
+        expect(client.apiToken).toBe(token);
+    });
 });
