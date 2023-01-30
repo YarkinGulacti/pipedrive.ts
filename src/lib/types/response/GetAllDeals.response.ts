298d917079d9648c5831d587500f80b1dcec95ab
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2023-01-30 19:18:25 +0300

    response type fix.

--- src/lib/types/response/GetAllDeals.response.ts
+++ src/lib/types/response/GetAllDeals.response.ts
@@ -4,12 +4,14 @@ export interface GetAllDealsResponse {
     success?: boolean;
     data?: Array<Deal>;
     additional_data?: {
-        /** @description Pagination start */
-        start?: number;
-        /** @description Items shown per page */
-        limit?: number;
-        /** @description If there are more list items in the collection than displayed or not */
-        more_items_in_collection?: boolean;
+        pagination?: {
+            /** @description Pagination start */
+            start?: number;
+            /** @description Items shown per page */
+            limit?: number;
+            /** @description If there are more list items in the collection than displayed or not */
+            more_items_in_collection?: boolean;
+        };
     };
     related_objects?: {
         /** @description The user who is associated with the deal */
